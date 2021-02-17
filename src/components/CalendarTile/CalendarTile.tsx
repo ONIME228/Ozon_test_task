d3951c1ee333ffc188fdf49be7ca9a05f732d8b9
jeanvania <naviyear.ikota@gmail.com>
2021-02-17 10:55:49 +0300

    HECK ME...

--- src/components/CalendarTile/CalendarTile.tsx
+++ src/components/CalendarTile/CalendarTile.tsx
@@ -6,54 +6,41 @@ import { getListOfDays, getWeekNumber, getMonthYear, } from '@/utils/utils';
 import { MyStore } from '@/store/store';
 import styles from './CalendarTile.css?module'
 
-interface ImouseEvent {
-    target: HTMLElement
-}
 
-const { calendarBlock, daysWeek, calendarTitle, days, chosenDay, filledDay } = styles;
+
+const { calendarBlock, daysWeek, calendarTitle, days, } = styles;
 const listOfDays = getListOfDays();
 const listOfEmptyDays = getWeekNumber();
 const month = getMonthYear();
 const weekDays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс',];
-
-// const getWeekDays = () => listOfEmptyDays.length ? listOfEmptyDays.map(() => <UnitCalendar id={''} text={''} />) : ''; 
 
 @Component
 export default class CalendarTile extends VueComponent {
 
     public store: MyStore = useStore(this.$store)
 
-    // Понимаю, что при любой смене структуре - обработчик упадет, но все же решил 
-    //, что добавить один тяжелый обработчик лучше, чем добавлять кучу маленьких 
-    handleClick(event: ImouseEvent) {
-        if (/span/i.test(event.target.tagName)) {
-            const element = document.getElementById(String(this.store.idToHighlight));
-            if (element) element.classList.remove(styles.chosenDay);
-            this.store.updateWithId(Number(event.target.getAttribute('id')));
-            event.target.classList.add(styles.chosenDay);
+    getClassName(day: number) {
+        if (day === listOfDays[this.store.idToHighlight - 1]) {
+            return styles.chosenDay;
         }
-    }
+        if (this.store.data[day] && this.store.data[day].length) {
+            return styles.filledDay;
+        }
+        return '';
+    };
 
 
     render() {
-        const { store: { idToHighlight, data } } = this;
-
         return (
-            < article class={calendarBlock}>
+            <article class={calendarBlock}>
                 <h5 class={calendarTitle}> {month} </h5>
                 <div class={daysWeek}>
-                    {weekDays.map((day: string) => <UnitCalendar text={String(day)} />)}
+                    {weekDays.map((day: string) => <UnitCalendar key={day} text={String(day)} />)}
                 </div>
-                <div class={days} onClick={this.handleClick} >
-                    {listOfEmptyDays.length ? listOfEmptyDays.map(() => <UnitCalendar id={''} text={''} />) : ''}
-                    {/* {getWeekDays} */}
+                <div class={days} >
+                    {listOfEmptyDays.length ? listOfEmptyDays.map((el: number) => <UnitCalendar key={String(el)} id={''} text={''} />) : ''}
                     {listOfDays.map((day: number) => {
-                        if (day === listOfDays[idToHighlight - 1]) {
-                            return <UnitCalendar key={String(day)} id={day} class={chosenDay} text={String(day)} />
-                        } else if (data[day] && data[day].length) {
-                            return <UnitCalendar key={String(day)} id={day} class={filledDay} text={String(day)} />
-                        }
-                        return <UnitCalendar key={String(day)} id={day} text={String(day)} />
+                        return <UnitCalendar class={this.getClassName(day)} key={String(day)} id={day} text={String(day)} />
                     })}
                 </div>
             </article >
--- src/components/UnitCalendar/UnitCalendar.tsx
+++ src/components/UnitCalendar/UnitCalendar.tsx
@@ -1,6 +1,7 @@
 import { Component, Prop } from 'vue-property-decorator';
 import { VueComponent } from '@/shims-vue';
-
+import { MyStore } from '@/store/store';
+import { useStore } from 'vuex-simple';
 
 //TSX Interface
 interface Props {
@@ -8,18 +9,26 @@ interface Props {
     id?: number | string,
 }
 
+
 @Component
 export default class UnitCalendar extends VueComponent<Props>{
+    public store: MyStore = useStore(this.$store)
     @Prop() private text!: string;
     @Prop() private id!: number;
 
+    handleClick() {
+        this.store.updateWithId(Number(this.id));
+    }
+
     render() {
-        const { text, id } = this;
+        const { text, id, handleClick } = this;
+
+
         if (!id && !text) {
             return <div></div>
         } else {
             return (
-                <span id={id} >{text}</span>
+                <span onClick={handleClick} id={id} >{text} </span>
             )
         }
 
